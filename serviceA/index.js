e0d9f0d91d0f7978096cd31e9ce4e0b9ba800049
Eik Emil Christensen <me@eikc.dk>
2018-10-28 13:55:17 +0100

    testing build triggers (#3)

--- serviceA/index.js
+++ serviceA/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+console.log('feature awesome')
+
 app.get('/', (req, res) => res.send('Hello World from serviceA!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
