bfe7d6b1035979098f91c586cb27720582285b1e
Eik Emil Christensen <me@eikc.dk>
2018-10-28 15:18:41 +0100

    testing testing (#5)

--- serviceB/index.js
+++ serviceB/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+console.log('new feature implemented');
+
 app.get('/', (req, res) => res.send('Hello World from service b - now with extra feature!!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
